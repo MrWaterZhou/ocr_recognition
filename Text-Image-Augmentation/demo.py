b443404f0d0def723255a013322e30d4037ccc8c
Wuxialang zhou <you@example.com>
2022-03-13 19:19:47 +0800

    x

--- Text-Image-Augmentation/demo.py
+++ Text-Image-Augmentation/demo.py
@@ -1,7 +1,7 @@
 import Augment
 import cv2
 import numpy as np
-
+import sys
 
 '''
 The code is for OpenCV format.
@@ -15,14 +15,13 @@ img = Image.open("The Path to the image")
 img = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
 '''
 
-im = cv2.imread("pic/demo.png")
-im = cv2.resize(im, (200, 64))
+im = cv2.imread(sys.argv[1])
+# im = cv2.resize(im, (200, 64))
 cv2.imshow("im_CV", im)
-for i in range(5000):
-	im_Distort = Augment.GenerateDistort(im, 4)
-	cv2.imshow("im_Distort", im_Distort)
-	im_Stretch = Augment.GenerateStretch(im, 4)
-	cv2.imshow("im_Stretch", im_Stretch)
-	im_Perspective = Augment.GeneratePerspective(im)
-	cv2.imshow("im_Perspective", im_Perspective)
-	cv2.waitKey(1)
+for i in range(5):
+    im_Distort = Augment.GenerateDistort(im, 4)
+    cv2.imwrite("im_Distort_{}.jpg".format(i), im_Distort)
+    im_Stretch = Augment.GenerateStretch(im, 4)
+    cv2.imwrite("im_Stretch_{}.jpg".format(i), im_Stretch)
+    im_Perspective = Augment.GeneratePerspective(im)
+    cv2.imwrite("im_Perspective_{}.jpg".format(i), im_Perspective)
