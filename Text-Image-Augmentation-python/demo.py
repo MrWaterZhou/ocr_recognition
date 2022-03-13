9a3eeb51432a57aaaadac9737239e3adfa254675
Wuxialang zhou <you@example.com>
2022-03-13 19:56:25 +0800

    x

--- Text-Image-Augmentation-python/demo.py
+++ Text-Image-Augmentation-python/demo.py
@@ -4,6 +4,7 @@
 import cv2
 import imageio
 from augment import distort, stretch, perspective
+import sys
 
 
 def create_gif(image_list, gif_name, duration=0.1):
@@ -15,8 +16,8 @@ def create_gif(image_list, gif_name, duration=0.1):
 
 
 if __name__ == '__main__':
-    im = cv2.imread("imgs/demo.png")
-    im = cv2.resize(im, (200, 64))
+    im = cv2.imread(sys.argv[1])
+    # im = cv2.resize(im, (200, 64))
     cv2.imshow("im_CV", im)
     distort_img_list = list()
     stretch_img_list = list()
@@ -24,16 +25,16 @@ if __name__ == '__main__':
     for i in range(12):
         distort_img = distort(im, 4)
         distort_img_list.append(distort_img)
-        cv2.imshow("distort_img", distort_img)
+        # cv2.imshow("distort_img", distort_img)
 
         stretch_img = stretch(im, 4)
-        cv2.imshow("stretch_img", stretch_img)
+        # cv2.imshow("stretch_img", stretch_img)
         stretch_img_list.append(stretch_img)
 
         perspective_img = perspective(im)
-        cv2.imshow("perspective_img", perspective_img)
+        # cv2.imshow("perspective_img", perspective_img)
         perspective_img_list.append(perspective_img)
-        cv2.waitKey(100)
+        # cv2.waitKey(100)
 
     create_gif(distort_img_list, r'imgs/distort.gif')
     create_gif(stretch_img_list, r'imgs/stretch.gif')
